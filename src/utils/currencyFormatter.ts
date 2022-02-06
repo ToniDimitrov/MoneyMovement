b08766df35a67f6147f23c779d8e6644cc7f0014
Toni Beliyashki <toni.beliyashki@gmail.com>
2022-02-06 23:12:31 +0200

    removed unneeded file.

--- src/utils/currencyFormatter.ts
+++
@@ -1,6 +0,0 @@
-export const getCurrencyFormatter = (currency: string): Intl.NumberFormat => {
-  return new Intl.NumberFormat(undefined, {
-    style: 'currency',
-    currency: currency,
-  });
-};
